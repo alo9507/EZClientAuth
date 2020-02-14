4b70e7ad3bf56625fa8e24bf2567e90fc6e546c0
Andrew <alo42@georgetown.edu>
2020-02-14 16:14:27 -0500

    reordered params in EZAuth to reflect tutorial

--- EZClientAuth/EZClientAuth/EZAuth.swift
+++ EZClientAuth/EZClientAuth/EZAuth.swift
@@ -12,14 +12,14 @@ import Foundation
 /// Holds the singleton AuthManager with the single synchronized AuthSession
 /// We prefix with EZ to prevent naming collisions with other libraries
 public struct EZAuth {
+    /// The singleton AuthManager to be used for all authentication CRUD
+    static public let manager: AuthMananger = EZAuthManager()
+    
     /// The client calls configure to method inject their choice of RemoteAuthProvider
     /// - Parameter authProvider: an enum holding the RemoteAuthProvider implementation as an associated value
     static public func configure(for authProvider: AuthProviderConfiguration) {
         manager.configure(for: authProvider)
     }
-    
-    /// The singleton AuthManager to be used for all authentication CRUD
-    static public let manager: AuthMananger = EZAuthManager()
     
     /// Convenience getter for the AuthSession held on the singleton AuthManager
     static public var session: AuthSession? {
